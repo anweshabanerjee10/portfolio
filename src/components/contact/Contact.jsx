41ec4f8b9132bb31aa3b6bd1259f2e67bbc884c1
Anwesha <anwesha.banerjee.cse23@heritageit.edu.in>
2023-07-06 14:34:50 +0530

    contact file updated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,30 @@
 import "./Contact.scss";
 
+import { useState } from "react";
+
 const Contact = () => {
-  return <div className="contact" id="contact"></div>;
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="/assests/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP :)</span>}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
